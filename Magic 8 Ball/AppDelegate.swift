da261a9ae00abfbad8143b474b41e6df197beb1a
Joanna Szaban <joannaszaban@MacBook-Pro-Joanna.local>
2023-03-14 09:22:51 +0100

    Initial Commit

---
+++ Magic 8 Ball/AppDelegate.swift
@@ -0,0 +1,37 @@
+//
+//  AppDelegate.swift
+//  Magic 8 Ball
+//
+//  Created by Joanna Szaban on 11/06/2022.
+
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+      
+        return true
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        
+    }
+
+    
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        
+    }
+
+
+}
+
